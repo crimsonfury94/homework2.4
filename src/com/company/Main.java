da9f6e7b26f1a9cc12115446f6ed9ba18be0ccbb
Dmitry <leon1861@yandex.ru>
2022-05-31 19:59:37 +0700

    готовое ДЗ

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SignIn user = new SignIn("Naruto015" , "qwer_Ty1", "qwer_Ty");
+        SignIn user = new SignIn("Naruto015", "qwer_Ty1", "qwer_Ty");
         SignIn.signInAcc(user.getLogin(), user.getPassword(), user.getConfirmPassword());
-
 
 
     }
